83d8e95cd0d1e9bf46254e41a0c086d65289f132
Julian Riedinger <julianriedinger27@gmail.com>
2022-10-16 20:12:08 -0300

    primera version chat

---
+++ public/main.js
@@ -0,0 +1,128 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js';
+import {
+	getDatabase,
+	onChildAdded,
+	ref,
+	query,
+	push,
+	orderByChild,
+} from 'https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js';
+
+import {
+	getAuth,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut,
+	onAuthStateChanged,
+} from 'https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyDld7zxFOEC7VuPiCFtKEbSvuOcpt0QWa8',
+	authDomain: 'choriweb-9f1cc.firebaseapp.com',
+	databaseURL: 'https://choriweb-9f1cc-default-rtdb.firebaseio.com',
+	projectId: 'choriweb-9f1cc',
+	storageBucket: 'choriweb-9f1cc.appspot.com',
+	messagingSenderId: '429015558142',
+	appId: '1:429015558142:web:4a95bb2bf2b406f2e582af',
+	measurementId: 'G-6ZRJ9ESF33',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+const auth = getAuth();
+
+const form = document.querySelector('form');
+const btnGoogle = document.querySelector('.google-btn');
+const textoLog = document.querySelector('.textoLog');
+const desloguear = document.querySelector('.desloguear');
+const listaMensajes = document.querySelector('.list-group');
+const nombre = document.querySelector('#nombre');
+const main = document.querySelector('main');
+const loading = document.querySelector('.loading');
+
+const msgRef = query(ref(db, 'mensajes/'), orderByChild('fecha'));
+
+const agregarAlChat = (data) => {
+	listaMensajes.innerHTML += `
+    <li class="list-group-item d-flex gap-2 align-items-center" id='${data.fecha}'>
+		<img src="${data.photo}" alt="${data.nombre}" />
+		${data.nombre}: ${data.mensaje}</li>
+  `;
+
+	document
+		.getElementById(data.fecha)
+		.scrollIntoView({ block: 'end', behavior: 'smooth' });
+};
+
+const agregarADatabase = (mensaje) => {
+	const objMensaje = {
+		nombre: auth.currentUser.displayName,
+		photo: auth.currentUser.photoURL,
+		email: auth.currentUser.email,
+		mensaje: mensaje,
+		fecha: Date.now(),
+	};
+	push(msgRef, objMensaje);
+};
+
+const signIn = () => {
+	const provider = new GoogleAuthProvider();
+	auth.languageCode = 'es';
+	signInWithPopup(auth, provider).then((result) => {
+		nombre.value = result.user.displayName;
+		form.classList.remove('d-none');
+		btnGoogle.classList.add('d-none');
+		textoLog.classList.add('d-none');
+		console.log('Auth en signin', auth);
+	});
+};
+
+const logout = () => {
+	signOut(auth).then(() => {
+		form.classList.add('d-none');
+		btnGoogle.classList.remove('d-none');
+		textoLog.classList.remove('d-none');
+	});
+};
+
+onChildAdded(msgRef, (snap) => {
+	const data = snap.val();
+	agregarAlChat(data);
+});
+
+console.log('getAuth', auth);
+
+form.addEventListener('submit', (evt) => {
+	evt.preventDefault();
+	const mensaje = document.querySelector('#mensaje');
+	if (mensaje.value !== '') {
+		agregarADatabase(mensaje.value);
+		mensaje.value = '';
+	} else alert('Es necesario ingresar nombre y mensaje');
+});
+
+btnGoogle.addEventListener('click', signIn);
+
+desloguear.addEventListener('click', logout);
+
+onAuthStateChanged(auth, (userLog) => {
+	if (userLog) {
+		nombre.value = auth.currentUser.displayName;
+		form.classList.remove('d-none');
+		btnGoogle.classList.add('d-none');
+		textoLog.classList.add('d-none');
+		loading.classList.add('d-none');
+		main.classList.remove('d-none');
+	} else {
+		loading.classList.add('d-none');
+		main.classList.remove('d-none');
+	}
+});
